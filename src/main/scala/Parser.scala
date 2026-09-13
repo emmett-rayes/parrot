2eee108b95375a02368744486501ae2a50d49afa
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-13 16:05:19 +0200

    defined Parser

---
+++ src/main/scala/Parser.scala
@@ -0,0 +1,14 @@
+package parrot
+
+import scala.util.Try
+
+/** A Parser over `String` input with `Throwable` failures.
+  *
+  * A parser of type *P*[*A*,*B*] maps an input *A* and a `String` to either a failure or a result *B* paired with the
+  * unconsumed remainder of the `String`.
+  *
+  * Implemented as the Kleisli profunctor of the state monad over the `Try` monad, where
+  *   - *P*[*A*,*B*] = *A* → `String` → ((*B* × `String`) + `Throwable`) is the object part
+  *   - *P*(*f*,*g*) = *f* ; - ; ((*g* × *id*) + *id*) is the morphism part
+  */
+type Parser = Kleisli[StateT[String, Try]]
