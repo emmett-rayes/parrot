2ae7ea5491e692361644d70556f7f91389505a06
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-13 15:44:25 +0200

    implemented Monad instances for StateT

--- src/main/scala/StateT.scala
+++ src/main/scala/StateT.scala
@@ -20,4 +20,28 @@ object StateT {
         s => self(s).map((result, state) => (f(result), state))
       }
   }
+
+  /** Every monad *M* induces a monad *T*, where
+    *   - *T*[*A*] = *S* → *M*[*A* × *S*] are the elements
+    *   - *η*(*a*) = ⟨*a*,*id*⟩ ; *η*' is the unit
+    *   - *μ*(*t*) = *t* ; *M*(*ev*) ; *μ*' is the multiplication
+    *
+    * Here *η*' and *μ*' are the unit and the multiplication of *M* and *ev*: *T*[*A*] × *S* → *M*[*A* × *S*] is the
+    * underlying function.
+    */
+  given StateTIsMonad: [S, M[_]: Monad] => StateT[S, M] is Monad {
+    def unit[A](a: A): StateT[S, M][A] = {
+      s => M.unit((a, s))
+    }
+
+    extension [A](self: StateT[S, M][StateT[S, M][A]])
+      def flatten: StateT[S, M][A] = {
+        s => self(s).flatMap((result, state) => result(state))
+      }
+
+    extension [A](self: StateT[S, M][A])
+      def map[B](f: A => B): StateT[S, M][B] = {
+        s => self(s).map((result, state) => (f(result), state))
+      }
+  }
 }
