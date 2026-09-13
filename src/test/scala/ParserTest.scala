122c362ae46d78d2a974b211cf61d1674fb7863e
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-13 20:22:05 +0200

    added ParserAlgebra.zip tests for Parser

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -60,4 +60,61 @@ class ParserTest extends AnyFunSuite {
     val parser = P.failure[Unit, String]
     assert(parser.run("").isFailure)
   }
+
+  test("andThen sequences two successful parsers") {
+    val p1     = P.literal("hello")
+    val p2     = P.success[String, Int](42)
+    val parser = p1 >> p2
+    assert(parser.run("hello world") == Success((result = 42, state = " world")))
+  }
+
+  test("andThen chains parsers consuming subsequent input") {
+    val p1     = P.literal("hello")
+    val bridge = P.success[String, Unit](())
+    val p2     = P.literal(" world")
+    val parser = p1 >> bridge >> p2
+    assert(parser.run("hello world!") == Success((result = " world", state = "!")))
+  }
+
+  test("andThen fails if the first parser fails") {
+    val p1     = P.literal("hello")
+    val p2     = P.success[String, Int](42)
+    val parser = p1 >> p2
+    assert(parser.run("goodbye").isFailure)
+  }
+
+  test("andThen fails if the second parser fails") {
+    val p1     = P.literal("hello")
+    val p2     = P.failure[String, Int]
+    val parser = p1 >> p2
+    assert(parser.run("hello world").isFailure)
+  }
+
+  test("zip sequences two successful parsers pairing results") {
+    val p1     = P.literal("hello")
+    val p2     = P.literal(" world")
+    val parser = p1 ** p2
+    assert(parser.run(((), ()), "hello world!") == Success((result = ("hello", " world"), state = "!")))
+  }
+
+  test("zip passes semantic inputs to respective parsers") {
+    val p1     = P.success[Int, String]("first")
+    val p2     = P.success[Double, Boolean](true)
+    val parser = p1 ** p2
+    assert(parser.run((1, 2.0), "input") == Success((result = ("first", true), state = "input")))
+  }
+
+  test("zip fails if the first parser fails") {
+    val p1     = P.failure[Unit, String]
+    val p2     = P.literal(" world")
+    val parser = p1 ** p2
+    assert(parser.run(((), ()), "hello world").isFailure)
+  }
+
+  test("zip fails if the second parser fails") {
+    val p1     = P.literal("hello")
+    val p2     = P.failure[Unit, String]
+    val parser = p1 ** p2
+    assert(parser.run(((), ()), "hello world").isFailure)
+  }
 }
