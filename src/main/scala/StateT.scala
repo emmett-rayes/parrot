76592106e44ecf8e351aab308c50bb7e25d88b31
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-13 15:39:53 +0200

    implemented Functor instances for StateT

--- src/main/scala/StateT.scala
+++ src/main/scala/StateT.scala
@@ -7,3 +7,17 @@ package parrot
   *   - *T*(*f*) = - ; *M*(*f* × *id*) is the morphism part
   */
 type StateT[S, M[_]] = [A] =>> S => M[(result: A, state: S)]
+
+object StateT {
+
+  /** Every functor *F* induces a functor *T*, where
+    *   - *T*[*A*] = *S* → *F*[*A* × *S*] is the object part
+    *   - *T*(*f*) = - ; *F*(*f* × *id*) is the morphism part
+    */
+  given StateTIsFunctor: [S, F[_]: Functor] => StateT[S, F] is Functor {
+    extension [A](self: StateT[S, F][A])
+      def map[B](f: A => B): StateT[S, F][B] = {
+        s => self(s).map((result, state) => (f(result), state))
+      }
+  }
+}
