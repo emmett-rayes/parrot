064228fb9c1726cf0fe511c0f14331009064a667
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-13 15:32:32 +0200

    implemented Profunctor instances for Kleisli

--- src/main/scala/Kleisli.scala
+++ src/main/scala/Kleisli.scala
@@ -6,4 +6,18 @@ package parrot
   *   - *K*[*A*,*B*] = *A* → *M*[*B*] is the object part
   *   - *K*(*f*,*g*) = *f* ; - ; *M*(*g*) is the morphism part
   */
-type Kleisli[M[_]] = [A, B] =>> (M is Monad) ?=> A => M[B]
+type Kleisli[M[_]] = [A, B] =>> A => M[B]
+
+object Kleisli {
+
+  /** Every functor *F* induces a profunctor *K*, where
+    *   - *K*[*A*,*B*] = *A* → *F*[*B*] is the object part
+    *   - *K*(*f*,*g*) = *f* ; - ; *F*(*g*) is the morphism part
+    */
+  given KleisliIsProfunctor: [F[_]: Functor] => Kleisli[F] is Profunctor {
+    extension [A, B](self: Kleisli[F][A, B])
+      def dimap[C, D](f: C => A, g: B => D): Kleisli[F][C, D] = {
+        c => self(f(c)).map(g)
+      }
+  }
+}
