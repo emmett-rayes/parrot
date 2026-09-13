e1977e1f000766bc13e1b6b9d4fa0b415dd6a969
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-13 12:33:52 +0200

    defined Profunctor

---
+++ src/main/scala/Profunctor.scala
@@ -0,0 +1,35 @@
+package parrot
+
+/** An endoprofunctor over **Type**.
+  *
+  * Represents a functor *P*: **Type**^*op* × **Type** → **Type**, where
+  *   - *P*[*A*,*B*] is the object part, contravariant in *A* and covariant in *B*
+  *   - *P*(*f*,*g*) is the morphism part
+  */
+trait Profunctor {
+  type Self[_, _]
+  type P = Self
+
+  extension [A, B](self: P[A, B])
+    /** Maps a pair of morphisms *f*: *C* → *A*, *g*: *B* → *D* to a morphism *P*[*A*,*B*] → *P*[*C*,*D*].
+      *
+      * Functoriality:
+      *   - identity: *P*(*id*,*id*) = *id*
+      *     - `self.dimap(identity, identity) == self`
+      *   - composition: *P*(*f*,*g*) ∘ *P*(*f*',*g*') = *P*(*f*' ∘ *f*,*g* ∘ *g*')
+      *     - `self.dimap(f, g).dimap(f2, g2) == self.dimap(f2 andThen f, g andThen g2)`
+      */
+    def dimap[C, D](f: C => A, g: B => D): P[C, D]
+
+  extension [A, B](self: P[A, B])
+    /** Maps a morphism *f*: *C* → *A* to a morphism *P*[*A*,*B*] → *P*[*C*,*B*]. */
+    def lmap[C](f: C => A): P[C, B] = {
+      self.dimap(f, identity[B])
+    }
+
+  extension [A, B](self: P[A, B])
+    /** Maps a morphism *g*: *B* → *D* to a morphism *P*[*A*,*B*] → *P*[*A*,*D*]. */
+    def rmap[D](g: B => D): P[A, D] = {
+      self.dimap(identity[A], g)
+    }
+}
