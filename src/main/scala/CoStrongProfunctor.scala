aeb7c34882990d089986ad57bdf48e4f2b0ca1db
Emmett Rayes <emmett.rayes@icloud.com>
2026-09-14 09:52:00 +0200

    defined CoStrongProfunctor

---
+++ src/main/scala/CoStrongProfunctor.scala
@@ -0,0 +1,40 @@
+package parrot
+
+/** A costrong endoprofunctor over **Type**.
+  *
+  * Represents a profunctor *P*: **Type**^*op* × **Type** → **Type** equipped with a cotensorial costrength with respect to
+  * the coproduct + on **Type**, where
+  *   - *left*: *P*[*A*,*B*] → *P*[*A* + *C*, *B* + *C*] is the left costrength
+  *   - *right*: *P*[*C*,*D*] → *P*[*A* + *C*, *A* + *D*] is the right costrength
+  */
+trait CoStrongProfunctor extends Profunctor {
+  type Self[_, _]
+  type P = Self
+
+  extension [A, B](self: P[A, B])
+    /** Maps an element in *P*[*A*,*B*] to an element in *P*[*A* + *C*, *B* + *C*].
+      *
+      * Unitality:
+      *   - identity: *P*(ρ, ρ⁻¹) ∘ *left* = *id*
+      *     - `self.left[Nothing].dimap(Left(_), _.merge) == self`
+      *
+      * Associativity:
+      *   - *P*(α, α⁻¹) ∘ *left* ∘ *left* = *left*
+      *     - `self.left[C].left[D].dimap({ case Left(Left(a)) => Left(a); case Left(Right(c)) => Right(Left(c)); case Right(d) => Right(Right(d)) }, { case Left(b) => Left(Left(b)); case Right(Left(c)) => Left(Right(c)); case Right(Right(d)) => Right(d) }) == self.left[Either[C, D]]`
+      */
+    def left[C]: P[Either[A, C], Either[B, C]]
+
+  extension [C, D](self: P[C, D])
+    /** Maps an element in *P*[*C*,*D*] to an element in *P*[*A* + *C*, *A* + *D*]. */
+    def right[A]: P[Either[A, C], Either[A, D]] = {
+      self.left[A].dimap(_.swap, _.swap)
+    }
+}
+
+object CoStrongProfunctor {
+
+  /** Summons the `CoStrongProfunctor` instance of `P`. */
+  def apply[P[_, _]: CoStrongProfunctor]: P is CoStrongProfunctor = {
+    summon
+  }
+}
